2ea98fad843db44d592b5a2a32cc2b6814ce777f
AnaMasFlavia <ana.moraes.pb@compasso.com.br>
2022-05-19 15:04:32 -0300

    Adicionando no index.js uma rota para buscar pela api

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const api = require("./api");
+
 const express = require("express");
 
 const server = express();
@@ -9,3 +11,12 @@ server.listen(8000);
 server.get("/", (req,res)=>{
     return res.send({message: "AGORA VAI PORRA"});
 });
+
+server.get("/dvd", async (req,res)=>{
+    try{
+        const { data }  = await api.get("/dvd/1");
+        return res.send({name: data.name});
+    }catch(error){
+        res.send({error: error.message});
+    }
+})
