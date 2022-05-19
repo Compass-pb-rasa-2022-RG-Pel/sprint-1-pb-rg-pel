b91a9c4b3795746d7d26300a6e11c88e533ddba7
AnaMasFlavia <ana.moraes.pb@compasso.com.br>
2022-05-19 20:43:14 -0300

    Algumas alterações para testes de algumas APIs conhecidas

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-const api = axios.creat({
-    baseURL: 'https://api.apitore.com/api/22',
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/movie/343611?api_key=6357cecb3e96ad809c27f13a9a84d7f9',
 });
 
 module.exports = api;
--- index.js
+++ index.js
@@ -12,11 +12,15 @@ server.get("/", (req,res)=>{
     return res.send({message: "AGORA VAI PORRA"});
 });
 
-server.get("/dvd", async (req,res)=>{
+server.get("/movie", async (req,res)=>{
+    const { id } = req.params;
     try{
-        const { data }  = await api.get("/dvd/1");
+        const { data }  = await api.get('movie/1');
+        console.log(data);
         return res.send({name: data.name});
     }catch(error){
         res.send({error: error.message});
     }
-})
+});
+
+const API_key = '6357cecb3e96ad809c27f13a9a84d7f9';
