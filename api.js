ad3647534bdf5f8ebfb16403ef0d29f3d8c19797
tatius7 <tatielizsilveira@gmail.com>
2022-05-21 19:48:56 -0300

    mudança de api pokemon para api dog

--- api.js
+++ api.js
@@ -1,3 +1,37 @@
+function buscaDog() {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+        }
+    };
+            fetch('https://dog.ceo/api/breeds/image/random', options)
+                .then(response => {
+                    return response.json()
+                }).then(json => {
+                    console.log(json);
+                    display_image(json.message,
+                                 220, 
+                                 110, 
+                                 'JavaScriptImage');
+                    })
+                    
+    
+                }
+
+                function display_image(src, width, height, alt) {
+                    var a = document.createElement("img");
+                    a.src = src;
+                    a.width = width;
+                    a.height = height;
+                    a.alt = alt;
+                    document.body.appendChild(a);
+                }
+        
+
+
+/*async function pokemon() { 
+    
 const axios = require("axios");
 
 const api = axios.create({
@@ -5,8 +39,6 @@ const api = axios.create({
 });
 
 module.exports = api;
-
-async function pokemon() { 
 
 const express = require("express");
 const cors = require("cors");
