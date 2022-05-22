579ca34897dfb22c6b2ed07fdad29f38231f4977
Rodrigovaladao01 <rodrigo.bm1@hotmail.com>
2022-05-21 22:51:30 -0300

    adicionado arquivos

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen("3000", ()=>{
-    console.log("testando a porta 3000");
+app.listen("8080", ()=>{
+    console.log("testando a porta 8080");
 });
